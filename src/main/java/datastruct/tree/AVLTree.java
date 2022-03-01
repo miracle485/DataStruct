9cd163bd983e7481a40f6a61d707087e23a54dba
wangyilin8 <wangyilin8@xiaomi.com>
2022-03-01 15:55:41 +0800

    增加二叉搜索树注释，增加AVL树

---
+++ src/main/java/datastruct/tree/AVLTree.java
@@ -0,0 +1,4 @@
+package datastruct.tree;
+
+public class AVLTree {
+}
--- src/main/java/datastruct/tree/BinaryTree.java
+++ src/main/java/datastruct/tree/BinaryTree.java
@@ -34,6 +34,7 @@ public class BinaryTree {
 
     public TreeNode deleteNode(TreeNode root, int key) {
 
+        //寻找待删除的节点
         TreeNode parent = null;
         TreeNode delNode = root;
         while (delNode != null && delNode.val != key) {
@@ -44,28 +45,35 @@ public class BinaryTree {
                 delNode = delNode.left;
             }
         }
+        //如果没找到待删除的节点，直接返回
         if (delNode == null) {
             return root;
         }
 
+        //如果待删除节点的左子树是空的，简化为删除只有右子树的节点的情况
         if (delNode.left == null) {
             TreeNode result = deleteSingleNode(delNode, parent, delNode.right);
             if (parent == null) {
                 root = result;
             }
-        } else if (delNode.right == null) {
+        }//如果待删除节点的右子树是空的，简化为删除只有左子树的节点的情况
+        else if (delNode.right == null) {
             TreeNode result = deleteSingleNode(delNode, parent, delNode.right);
             if (parent == null) {
                 root = result;
             }
-        } else {
+        } //如果待删除节点左右子树都不为空
+        else {
             TreeNode left = delNode.right;
             TreeNode rParent = delNode;
+            //那么先去寻找待删除节点的后继节点，即右子树的最左节点
             while (left.left != null) {
                 rParent = left;
                 left = left.left;
             }
+            //此时将待删除节点的值替换为后继节点的值
             delNode.val = left.val;
+            //用后继节点的右子树替换后继节点的位置
             if (rParent.left == left) {
                 rParent.left = left.right;
             } else {
@@ -76,7 +84,16 @@ public class BinaryTree {
         return root;
     }
 
+    /**
+     * 删除只有一颗子树的情况
+     *
+     * @param del    待删除的节点
+     * @param parent 待删除节点的父节点
+     * @param dist   待删除节点的左子树或者右子树
+     * @return 返回删除后的待删除节点，方便待删除节点为根节点时的情况处理
+     */
     public TreeNode deleteSingleNode(TreeNode del, TreeNode parent, TreeNode dist) {
+        //如果父节点为空，说明当前节点为子节点
         if (parent == null) {
             del = dist;
         } else if (del == parent.left) {
